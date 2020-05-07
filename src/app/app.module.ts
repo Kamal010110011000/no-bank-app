9feac3f12151e8555cf5930eb19b0c453437941a
Kamal010110011000 <kamalsingh.4iv1@gmail.com>
2020-05-07 21:43:53 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './user/dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { UserProductComponent } from './user/dashboard/user-product/user-product.component';
+import { UserOrdersComponent } from './user/dashboard/user-orders/user-orders.component';
+import { UserHistoryComponent } from './user/dashboard/user-history/user-history.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    DashboardComponent,
+    ProductComponent,
+    UserProductComponent,
+    UserOrdersComponent,
+    UserHistoryComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
